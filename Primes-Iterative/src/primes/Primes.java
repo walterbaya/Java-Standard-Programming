70b9ffb93ecdc560769b2e0b03c9807867600df3
Walter Baya <walterbaya@yahoo.com>
2019-12-30 11:16:26 -0300

    se sube la version iterativo de primes

---
+++ Primes-Iterative/src/primes/Primes.java
@@ -0,0 +1,80 @@
+package primes;
+
+import java.util.Scanner;
+//la complejidad de este algoritmo es complejidad O(n)
+//este algoritmo es iterativo y mucho mas eficiente en terminos de memoria
+class Primes {
+
+    public static void main(String[] args) {
+        Scanner esc = new Scanner(System.in);
+        int N = esc.nextInt();
+        String s = primes(N);
+        System.out.println(s);
+    }
+
+    static String primes(int n) {
+        String s = "2 ";
+        int f = n;
+        int i = 2;
+        while (i <= f) {
+            switch (i % 10) {
+                case 0:
+                    i += 1;
+                    break;
+                case 1:
+                    if (isPrime(i)) {
+                        s = s + i + " ";
+                    }
+                    i += 2;
+                    break;
+                case 2:
+                    i += 1;
+                    break;
+                case 3:
+                    if(i==3){
+                        s = s + 3 + " " + 5 + " "; 
+                    
+                    }
+                    else if (isPrime(i)) {
+                        s = s + i + " ";
+                    }
+                    i += 4;
+                    break;
+                case 4:
+                    i += 3;
+                    break;
+                case 5:
+                    i += 2;
+                    break;
+                case 6:
+                    i += 1;
+                    break;
+                case 7:
+                    if (isPrime(i)) {
+                        s = s + i + " ";
+                    }
+                    i += 2;
+                    break;
+                case 8:
+                    i += 1;
+                    break;
+                default:
+                    if (isPrime(i)) {
+                        s = s + i + " ";
+                    }
+                    i += 2;
+                    break;
+            }
+        }
+        return s;
+    }
+
+    static boolean isPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
