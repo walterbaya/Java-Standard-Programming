6bfd7d11ddd238b20c7ba115bd09d1a15b2b8ed4
Walter Baya <walterbaya@yahoo.com>
2019-12-31 17:57:14 -0300

    Se agregan funcionalidades a los botones de cargar optativas

--- ProyectoUnaVentana/src/ProyectoDosVentanas/VentanaOptativasController.java
+++ ProyectoUnaVentana/src/ProyectoDosVentanas/VentanaOptativasController.java
@@ -1,27 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ProyectoDosVentanas;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author WalterAriel
- */
-public class VentanaOptativasController implements Initializable {
+public class VentanaOptativasController extends VentanaComputacionController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    static int puntosFaltantes = 12;
+    String nombreMateriaS = " ";
+
+    @FXML
+    TextField nombreMateria;
+    @FXML
+    TextField puntosMateria;
+    @FXML
+    TextField notaObtenida;
+    @FXML
+    TextField puntosRestantes;
+
+    @FXML
+    public void ingresarMateriaOptativa() {
+        nombreMateriaS = nombreMateriaS + nombreMateria.getText() + " ";
+        int puntosMateriaS = (int) Integer.valueOf(puntosMateria.getText());
+        double notaObtenidaS = Double.valueOf(notaObtenida.getText());
+        super.setSuma(notaObtenidaS);
+        super.IncrementarCantNotas();
+        puntosFaltantes = puntosFaltantes - puntosMateriaS;
+        puntosRestantes.setText(String.valueOf(puntosFaltantes));
+        System.out.println(puntosFaltantes);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        puntosRestantes.setDisable(true);
+    }
+
 }
