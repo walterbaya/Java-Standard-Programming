d18d061ce4c43ab9a2146e9afd39340bbc7ca729
walterbaya <walterbaya@yahoo.com>
2020-03-06 05:59:22 -0300

    I have created employee class and main class

---
+++ Java Standard Laboratorios/Laboratorio7/src/Employee.java
@@ -0,0 +1,15 @@
+
+public class Employee {
+	private String name;
+	private String identifier;
+	private int age;
+
+	/** remember: constructor's name should be same as class' name */
+
+	public Employee(String name, String identifier, int age) {
+		this.name = name;
+		this.age = age;
+		this.identifier = identifier;
+	}
+
+}
