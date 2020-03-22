5b66fec979541f72746ad84b0db1a93b50fdf3a8
walterbaya <walterbaya@yahoo.com>
2020-03-22 10:55:33 -0300

    se sube laboratorio7

--- Java Standard Laboratorios/Laboratorio7/src/Client.java
+++ Java Standard Laboratorios/Laboratorio7/src/Client.java
@@ -1,33 +1,36 @@
 
 public class Client {
-/** Ejercicio #2
-	Construir una clase llamada Cliente que tenga como atributos “razón social” y “dirección”. Respetar el encapsulamiento
-
-	Redefinir el método toString() para que retorne la razón social y la dirección
-
-	Construir una clase llamada ImpresorDeClientes
-
-	El código a desarrollar estará dentro del método main de esta clase
-
-	Instanciar 5 clientes y agregarlos a un ArrayList denominado losClientes
-
-	Utilizar un iterador para recorrer losClientes e imprimir los datos de cada uno en pantalla
- */
+	/**
+	 * 
+	 * Redefinir el método toString() para que retorne la razón social y la
+	 * dirección
+	 * 
+	 * Construir una clase llamada ImpresorDeClientes
+	 * 
+	 * El código a desarrollar estará dentro del método main de esta clase
+	 * 
+	 * Instanciar 5 clientes y agregarlos a un ArrayList denominado losClientes
+	 * 
+	 * Utilizar un iterador para recorrer losClientes e imprimir los datos de cada
+	 * uno en pantalla
+	 */
 	private String socialReason;
 	private String address;
-	
+
 	public void setAddress(String address) {
-		this.address=address;
+		this.address = address;
 	}
+
 	public String getAddress() {
 		return this.address;
 	}
+
 	public void setSocialReason(String socialReason) {
-		this.socialReason=socialReason;
+		this.socialReason = socialReason;
 	}
+
 	public String getSocialReason() {
 		return this.socialReason;
 	}
-	
-	
+
 }
